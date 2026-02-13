4214a96a710caf201c247470dc53bbf9553b7ec7
Julio Henrique <juliohm37@outlook.com>
2026-02-13 16:56:27 -0300

    otimização na renderização de blocks

--- Scripts/World.cs
+++ Scripts/World.cs
@@ -4,25 +4,74 @@ using System.Data;
 
 public partial class World : Node3D
 {
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		Chunk chunk = new Chunk();
 
-		for (int x = 0;x<Chunk.Size;x++)
-		for (int y=0;y<Chunk.Size;y++)
+		BuildChunkMesh(chunk);
+	}
+
+	void BuildChunkMesh(Chunk chunk)
+	{
+		var st = new SurfaceTool();
+		st.Begin(Mesh.PrimitiveType.Triangles);
+		
+		for (int x = 0; x < Chunk.Size; x++)
+		for( int y = 0; y < Chunk.Size; y++)
 		for (int z = 0; z < Chunk.Size; z++)
 		{
 			if (chunk.GetBlock(x, y, z) != 0)
 			{
-				var cube = new MeshInstance3D();
-				cube.Mesh = new BoxMesh();
-				
-				cube.Position = new Vector3(x,y,z);
-				
-				AddChild(cube);
+				AddCube(st, new Vector3(x, y, z));
 			}
 		}
+
+		var mesh = st.Commit();
+
+		var meshInstance = new MeshInstance3D();
+		meshInstance.Mesh = mesh;
+		
+		AddChild(meshInstance);
+		
+	}
+
+
+	void AddCube(SurfaceTool st, Vector3 pos)
+	{
+		Vector3[] v = new Vector3[]
+		{
+			pos + new Vector3(0, 0, 0),
+			pos + new Vector3(1, 0, 0),
+			pos + new Vector3(1, 1, 0),
+			pos + new Vector3(0, 1, 0),
+			
+			pos + new Vector3(0, 0, 1),
+			pos + new Vector3(1, 0, 1),
+			pos + new Vector3(1, 1, 1),
+			pos + new Vector3(0, 1, 1),
+		};
+
+		int[][] faces =
+		{
+			new[] { 0, 1, 2, 3 },
+			new[] { 5, 4, 6, 7 },
+			new[] { 4, 0, 3, 7 },
+			new[] { 1, 5, 6, 2 },
+			new[] { 3, 2, 6, 7 },
+			new[] { 4, 5, 1, 0 },
+		};
+
+		foreach (var f in faces)
+		{
+			st.AddVertex(v[f[0]]);
+			st.AddVertex(v[f[1]]);
+			st.AddVertex(v[f[2]]);
+			
+			st.AddVertex(v[f[0]]);
+			st.AddVertex(v[f[2]]);
+			st.AddVertex(v[f[3]]);
+		}
+		
 	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
